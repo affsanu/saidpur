49166bc88042e41673d86d7f3c0a5aec13e0b3f0
Sanu Islam <dropnow.sanu@gmail.com>
2023-09-09 18:18:27 +0600

    Ads all

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
+        <script async custom-element="amp-auto-ads"
+          src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+        </script>
       </head>
       <body className={bengali.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
