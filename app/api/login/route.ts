561c44167b3c921b0f2e9306aef5b17fa8a9cd1a
Sanu Islam <dropnow.sanu@gmail.com>
2023-09-16 14:03:52 +0600

    Prisma Login fixed

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -29,6 +29,7 @@ export async function POST(request: Request) {
         const hashedPassword = await bcrypt.hash(password, 12);
         const user = await prisma.user.create({
             data: {
+                name: "Admin",
                 email,
                 password: hashedPassword,
                 isAdmin: true,
